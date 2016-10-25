88478350eee282bf8d6f482c16471631fa8427c3
余阿伟 <awei.yu@2zhuzi.com>
2016-10-25 17:39:40 +0800

    创建

---
+++ README.md
@@ -0,0 +1,17 @@
+# cache-promise
+用于缓存promise对象，一般是某个http请求
+
+## 安装
+```
+npm install cache-promise
+```
+## 使用
+```
+import CachePromise from 'cache-promise'
+// 实例化
+// 参数1: promise方法 参数2: 缓存时效（毫秒）
+var accessToken = new CachePromise(() => httpClient.get('http://xxx.com/token'), 30000)
+// 调用实例对象的get方法。优先取缓存，过期重新获取
+accessToken.get()
+    .then(accessToken => console.log(accessToken))
+```
---
+++ cache-promise.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/**
+ * Created by awei on 2016/8/2.
+ */
+function Cache(getAfterCache, timeOut) {
+  this.cache = {
+    promise: '',
+    createTime: ''
+  };
+  this.getAfterCache = getAfterCache;
+  this.timeOut = timeOut;
+}
+Cache.prototype = {
+  get() {
+    return new Promise(resolve => {
+      var cache = this.cache;
+      if (cache.promise && new Date() - cache.createTime < this.timeOut) return resolve(cache.promise);
+      var promise = this.getAfterCache().then(rs => {
+        this.cache.createTime = new Date();
+        return rs;
+      });
+      this.set(promise);
+      resolve(promise);
+    });
+  },
+  set(cachePromise) {
+    this.cache.promise = cachePromise;
+    this.cache.createTime = new Date();
+  }
+};
+module.exports = Cache;
