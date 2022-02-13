f52baffa81f30bb56518b7bb0dd536b2e3cd16a9
aweiu <328080339@qq.com>
2022-02-13 21:04:22 +0800

    feat: support clearCache & forceUpdate

--- src/CachePromise.ts
+++ src/CachePromise.ts
@@ -9,8 +9,20 @@ export default class CachePromise<T = any> {
     this.expires = expires
   }
 
+  async forceUpdate() {
+    this.lastTime = new Date().getTime()
+    this.cache = this.promiseFun()
+    try {
+      return await this.cache
+    } catch (e) {
+      this.cache = undefined
+      throw e
+    }
+  }
+
   async get() {
-    const { cache, expires, lastTime, promiseFun } = this
+    const { cache, expires, lastTime } = this
+
     if (
       cache &&
       (expires === 0 || new Date().getTime() - (lastTime as number) < expires)
@@ -18,13 +30,11 @@ export default class CachePromise<T = any> {
       // 这里其实还可以粗暴地判断 this.cache 是否为空来实现如果上次任务失败了则重试，没失败则复用它的结果
       return cache
     }
-    this.lastTime = new Date().getTime()
-    this.cache = promiseFun()
-    try {
-      return await this.cache
-    } catch (e) {
-      this.cache = undefined
-      throw e
-    }
+
+    return this.forceUpdate()
+  }
+
+  clearCache() {
+    this.cache = undefined
   }
 }
